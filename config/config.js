3783233a43356d9d76f70f72cdc866d508719afa
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-10 03:10:48 +0900

    feat: dotenv

--- config/config.js
+++ config/config.js
@@ -1,24 +1,25 @@
-{
-    "development" : {
+require("dotenv").config()
+const env = process.env
+
+const development = {
     "username": "root",
     "password": "rkd785985",
     "database": "Lv5",
     "host": "express-database.clplypyzjltq.ap-northeast-2.rds.amazonaws.com",
     "dialect": "mysql"
-  },
-  "test": {
+  }
+  const test= {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  },
-  "production": {
+  }
+  const production = {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  }
   }
   
